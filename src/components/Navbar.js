a51117e8289605a1844d3372916f8e378ef9a120
Fathima Minha <minhasulaikar@gmail.com>
2025-03-04 03:18:22 +0530

    create MenuItems

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./Navbar.css";
 //import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
   render() {
