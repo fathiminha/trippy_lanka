34ab39935ea3b69db17097d41aa5cd11946dbf37
Fathima Minha <minhasulaikar@gmail.com>
2025-03-13 11:38:34 +0530

    add hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import './Hero.css';
+
+export default function Hero(){
+    return(
+        <>
+            <div className='hero'>
+                <img alt='HerpImg' src='' />
+            </div>
+            <div className='hero-text'>
+                <h1> Your Journey Your Story </h1>
+                <p> Choose Your Favourite Destination.</p>
+                <a href='/'> Travel Plan </a>
+            </div>
+        </>
+    )
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
+import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 
 export default function Home(){
     return(
         <>
             <Navbar />
-            <h1>This is Home</h1>
+            <Hero />
         </>
     )
 }
