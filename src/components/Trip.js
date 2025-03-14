ff2461de40dc12758d9fabf92770406009633c03
Fathima Minha <minhasulaikar@gmail.com>
2025-03-14 21:46:18 +0530

    add some styles

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -16,12 +16,12 @@ export default function Trip(){
                     text ="Rising dramatically from the heart of Sri Lanka, Sigiriya is an ancient rock fortress and a UNESCO World Heritage Site. Known as the 'Eighth Wonder of the World,' it boasts breathtaking frescoes, beautifully landscaped gardens, and panoramic views from the summit. Built by King Kashyapa in the 5th century, this architectural marvel is a must-visit for history lovers and adventure seekers alike."
                 />
                 <TripData 
-                    image={t2}
+                    image={t3}
                     heading = "Kandy"
                     text = "Nestled in the central hills, Kandy is a vibrant city rich in history, culture, and natural beauty. Home to the sacred Temple of the Tooth Relic, this UNESCO-listed city is a spiritual hub for Buddhists. Surrounded by lush greenery and the scenic Kandy Lake, it offers a perfect blend of tradition, breathtaking landscapes, and colonial charm. Visitors can also explore the Royal Botanical Gardens in Peradeniya or witness the colorful Esala Perahera festival, making Kandy a truly unforgettable destination."
                 />
                 <TripData 
-                    image={t3}
+                    image={t2}
                     heading = "Yala National Park"
                     text ="Yala National Park is Sri Lanka’s most famous wildlife sanctuary, known for its rich biodiversity and incredible safari experiences. Home to the world’s highest density of leopards, along with elephants, sloth bears, crocodiles, and exotic birds, Yala offers a thrilling glimpse into the island’s diverse wildlife. The park’s mix of lush forests, grasslands, and lagoons creates a stunning natural landscape for adventure seekers. Whether you're on a jeep safari or capturing breathtaking sunsets over the wilderness, Yala promises an unforgettable encounter with nature."
                 />
