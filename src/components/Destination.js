6817e1e0220829a384d6282b3ce08cc9c03df8a6
Fathima Minha <minhasulaikar@gmail.com>
2025-03-14 20:14:17 +0530

    make destination component as dynamic

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,29 +1,28 @@
+import './Destination.css'
+import DestinationData from './DestinationData';
 import i1 from '../assets/i1.jpg';
 import i2 from '../assets/i2.jpg';
-import './Destination.css'
+
 
 export default function Destination(){
     return(
         <div className="destination">
             <h1> Popular Destination </h1>
             <p>Tours give you the opportunity to see a lot, within a time frame </p>
-            <div className="first-des">
-                <div className="des-text">
-                    <h2> Ella </h2>
-                    <p> 
-                        Nestled in Sri Lanka’s lush hill country, <b>Ella</b> is a breathtaking destination known for its misty mountains, 
-                        rolling tea plantations, and stunning waterfalls. Whether you're hiking up <b>Little Adam’s Peak</b>, 
-                        exploring the iconic <b>Nine Arches Bridge</b>, or enjoying a scenic train ride through the hills, 
+            
+            <DestinationData
+                heading= "Ella"
+                text =" Nestled in Sri Lanka’s lush hill country, Ella is a breathtaking destination known for its misty mountains, 
+                        rolling tea plantations, and stunning waterfalls. Whether you're hiking up Little Adam’s Peak, 
+                        exploring the iconic Nine Arches Bridge, or enjoying a scenic train ride through the hills, 
                         Ella offers a perfect blend of adventure and relaxation. With its cool climate, charming cafes, 
                         and mesmerizing views, 
-                        it’s a must-visit paradise for nature lovers and explorers alike.
-                    </p>
-                </div>
-                <div className="image">
-                    <img alt="ella1" src={i1} />
-                    <img alt="ella2" src={i2} />
-                </div>
-            </div>
+                        it’s a must-visit paradise for nature lovers and explorers alike."
+               
+               img1={i1}
+               img2={i2}
+               />
         </div>
+        
     )
 }
---
+++ src/components/DestinationData.js
@@ -0,0 +1,24 @@
+import {Component} from 'react';
+import './Destination.css';
+import i1 from '../assets/i1.jpg';
+import i2 from '../assets/i2.jpg';
+
+class DestinationData extends Component{
+    render(){
+        return(
+            <div className="first-des">
+                <div className="des-text">
+                    <h2> {this.props.heading} </h2>
+                    <p> 
+                       {this.props.text}
+                    </p>
+                </div>
+                <div className="image">
+                <img alt="ella1" src={this.props.img1} />
+                <img alt="ella2" src={this.props.img2} />
+                </div>
+            </div>
+        )
+    }
+}
+export default DestinationData;
