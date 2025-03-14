904583497395ddc7af0123d7940ff67f8ce554ea
Fathima Minha <minhasulaikar@gmail.com>
2025-03-14 20:46:29 +0530

    create trip component

---
+++ src/components/Trip.js
@@ -0,0 +1,10 @@
+import './Trip.css';
+
+export default function Trip(){
+    return(
+        <div className='trip'>
+            <h1> Recent Trips </h1>
+            <p> You can discover unique destinations using Google Maps</p>
+        </div>
+    )
+}
