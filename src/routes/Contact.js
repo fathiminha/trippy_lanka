9d21171aae716c024cc38795c6f78e70f302a2ef
Fathima Minha <minhasulaikar@gmail.com>
2025-03-14 23:27:18 +0530

    contact form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar';
 import ContactImg from '../assets/5.jpg';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
+import ContactForm from '../components/ContactForm';
 
 
 export default function Contact(){
@@ -14,6 +15,7 @@ export default function Contact(){
                 title= "Contact"
                 btnClass="hide"
             />
+            <ContactForm />
             <Footer />
         </>
     )
